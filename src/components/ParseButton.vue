cc6f8c9e87cfb8a819c06734062e2f734afb7cae
Ben Baryo <--all>
2026-05-10 00:28:06 +0300

    Remove unused focusExploreNodes parameter from parseContent call in ParseButton.vue for improved clarity.

--- src/components/ParseButton.vue
+++ src/components/ParseButton.vue
@@ -103,7 +103,6 @@ onMounted(() => {
 
     store.shouldAutoParseInitialInput = false;
     parseContent({
-      focusExploreNodes: true,
       pulseCodeStructures: true,
     });
     return true;
